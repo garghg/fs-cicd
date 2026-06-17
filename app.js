1d70e2668951f698c6035f72c06724c2bb06c241
Haardik Garg <hghaardik@gmail.com>
2026-06-17 14:26:34 -0700

    add health check endpoint

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ app.get('/version', (req, res) => {
   res.send('1') // change this string to ensure a new version deployed
 })
 
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
 // get the port from env variable
 const PORT = process.env.PORT || 5001
 
