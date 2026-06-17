847b935c4045e316772bc6e3f529fdadbd30f20d
Haardik Garg <hghaardik@gmail.com>
2026-06-17 13:11:21 -0700

    added pokemon page test

--- e2e-tests/tests.spec.js
+++ e2e-tests/tests.spec.js
@@ -2,8 +2,13 @@ import { test, describe, expect } from '@playwright/test'
 
 describe('Pokedex', () => {
   test('front page can be opened', async ({ page }) => {
-     await page.goto('')
+    await page.goto('')
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
+  })
+  test('navigate to pokemon page', async ({ page }) => {
+    await page.goto('')
+    await page.getByRole('link', { name: 'ivysaur' }).click()
+    await expect(page.getByText('chlorophyll')).toBeVisible()
   })
 })
